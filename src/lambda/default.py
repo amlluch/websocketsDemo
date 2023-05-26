037ab630bd0ecd8d153d9725e003b0d11a435519
Alfonso Lluch <alfonso.lluch@kaizenreporting.com>
2023-05-26 09:19:36 +0100

    send to SQS from a lambda

--- src/lambda/default.py
+++ src/lambda/default.py
@@ -1,9 +1,17 @@
+import json
 from typing import Dict
+
+import boto3
+import os
 
 from aws_lambda_typing.context import Context
 
 
-def handler(event: Dict, context: Context) -> Dict:
-    print(event)
-
-    return {"statusCode": 200}
+def handler(event: Dict, context: Context):
+    sqs = boto3.client('sqs')
+    queue_url = os.environ['QUEUE_URL']
+    response = sqs.send_message(
+        QueueUrl=queue_url,
+        MessageBody=json.dumps(event)
+    )
+    return response
