a6abac01fe8d64ec7a1d6cee6c4d35d9f7c2f949
amlluch <alfonso.lluch@kaizenreporting.com>
2023-05-31 18:34:01 +0200

    adding more silly code

--- src/lambda/send_message.py
+++ src/lambda/send_message.py
@@ -6,5 +6,6 @@ from aws_lambda_typing.context import Context
 def handler(event: Dict, context: Context) -> Dict:
     print(event)
     print("to remove")
+    print("to be removed")
 
     return {"statusCode": 200}
