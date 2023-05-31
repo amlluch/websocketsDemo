6578032a4bb63420595af72872b8733a2f5be767
amlluch <alfonso.lluch@kaizenreporting.com>
2023-05-31 18:50:14 +0200

    removing silly code

--- src/lambda/send_message.py
+++ src/lambda/send_message.py
@@ -5,7 +5,5 @@ from aws_lambda_typing.context import Context
 
 def handler(event: Dict, context: Context) -> Dict:
     print(event)
-    print("to remove")
-    print("to be removed")
 
     return {"statusCode": 200}
