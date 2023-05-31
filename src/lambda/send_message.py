2b41de2aaaf2248fff54a80eff39e821b53d5d6a
amlluch <alfonso.lluch@kaizenreporting.com>
2023-05-31 16:32:56 +0200

    testing revert

--- src/lambda/send_message.py
+++ src/lambda/send_message.py
@@ -5,7 +5,5 @@ from aws_lambda_typing.context import Context
 
 def handler(event: Dict, context: Context) -> Dict:
     print(event)
-    print("Small change")
-    print("prueba para revertir")
 
     return {"statusCode": 200}
